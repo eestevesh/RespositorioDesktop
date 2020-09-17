9f370dd9cb708c0d085bb2b70606f37c494e3c3a
eestevesh <68910112+eestevesh@users.noreply.github.com>
2020-09-17 12:19:31 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -54,25 +54,26 @@ function showProducts(currentProductsArray) {
 
 
             htmlContentToAppend += `
-                    <div class="list-group-item list-group-item-action">
+                    <a href="product-info.html" class="list-group-item list-group-item-action">
                        <div class="row">
                             <div class="col-3">
                                 <img src="` + products.imgSrc + `" alt="` + products.name + ` " class="img-thumbnail">
                             </div>
-                            <div class="col">
-                                <div class=" d-flex w-100 justify-content-between">
-                                    <h4 class="mb-1">`+ products.name + `</h4>
-                                    <small class="text-muted">` + products.soldCount + ` artículos vendidos</small>
+                                <div class="col"> 
+                                    <div class=" d-flex w-100 justify-content-between">
+                                        <h4 class="mb-1">`+ products.name + `</h4>
+                                        <small class="text-muted">` + products.soldCount + ` artículos vendidos</small>
+                                    </div>
+                                    <div>
+                                        <p>`+ products.description + `</p>
+                                    </div>
+                                    <div>
+                                        <p>`+ products.currency + ` ` + products.cost + `</p>
+                                    </div>
+                                
                                 </div>
-                                <div>
-                                    <p>`+ products.description + `</p>
-                                </div>
-                                <div>
-                                    <p>`+ products.currency + ` ` + products.cost + `</p>
-                                </div>
-                            </div>
                         </div>
-                    </div>
+                    </a>
                     `
         }
 
